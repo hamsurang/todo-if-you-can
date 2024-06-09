7c56df856790a4e556c6ab690b2f7c1b7c3782f2
minchodang <minsu910725@gmail.com>
2024-06-09 16:43:47 +0900

    Feat: todoItem 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
 import "./App.css";
 import { Button } from "@chakra-ui/react";
+import { useSessionStorage } from "usehooks-ts";
 import { useUploadModal } from "./hooks/useUploadModal";
+import { TodoItem } from "./components/TodoItem";
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-  const { open } = useUploadModal();
+  const [todoList, setTodoList] = useSessionStorage("킹받두", [])
+  const {open} = useUploadModal();
 
   return (
     <div className="layout">
       <section className="todo-list">
         <h1 className="title">킹받do</h1>
+        <Button onClick={open}>
+          todo입력해do
+        </Button>
+        {todoList.map((todo, index) => (
+          <TodoItem
+            key={index}
+            todo={todo}
+            onToggle={(id) => {
+              setTodoList(
+                todoList.map((todo) => {
+                  if (todo.id === id) {
+                    return { ...todo, done: !todo.done };
+                  }
+                  return todo;
+                })
+              );
+            }}
+            onRemove={(id) => {
+              setTodoList(todoList.filter((todo) => todo.id !== id));
+            }}
+          />
+        ))}
       </section>
     </div>
   );
